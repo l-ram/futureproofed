64e5eb8c41dbdf3982ef2aa36de4b38d59071650
l-ram <larryramjutton@gmail.com>
2023-02-21 23:52:26 +0100

    wip

--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
+  <div class="dataView">
 
-    <div class="list" v-for="(headCategory, index) in listHeadCategory" :key="headCategory">
-      {{ index }}
-      <h1>{{ headCategory }}</h1>
+    <div class="list-bg">
+      <div v-for="(headCategory, index) in listHeadCategory" :key="headCategory">
+        <h3 class="list-head">{{ headCategory }}</h3>
 
-      <div class="sub-list" v-for="subCats in listSubCategories">
-        <div>
-          {{ subCats.title }}
+        <div v-for="subCats in listSubCategories">
+          <div class="sub-list">
+            {{ subCats.title }}
+          </div>
+          <div class="buildings" v-for="building in subCats.buildings" :key="building">
+            {{ building }}
+          </div>
+
         </div>
-        <div v-for="building in subCats.buildings" :key="building"
-        >
-          {{ building }}
-        </div>
-
 
 
       </div>
-
-
     </div>
-
     <DataForm />
+
 
   </div>
 </template>
@@ -44,8 +42,6 @@ const listSubCategories = [
     title: "Processes"
   }
 ]
-
-
 export default Vue.extend({
   name: "DataView",
   data() {
@@ -62,3 +58,42 @@ export default Vue.extend({
 });
 
 </script> 
+
+<style scoped>
+.dataView {
+  background-color: #cecece;
+  position: relative;
+  padding-left: 8%;
+  padding-top: 10%;
+}
+</style>
+
+<style>
+.list-bg {
+  border-radius: 15px;
+  width: 70%;
+}
+
+.list-head {
+  background-color: #f6f6f6;
+  position: relative;
+  border-radius: 5px 5px 0 0;
+  margin: 0.15em;
+}
+
+.sub-list {
+  position: relative;
+  background-color: #52d9915e;
+  border-radius: 5px 5px 0 0;
+}
+
+.buildings {
+  position: relative;
+  background-color: #dadada;
+  left: 5%;
+  width: 92%;
+  margin: 0.15em;
+  border-radius: 2px;
+}
+
+</style>
